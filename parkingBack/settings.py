224ffa0cbf1b03ff29702797cb7c693adc3b6a6b
3La221 <abacha3la2@gmail.com>
2024-05-20 19:13:28 +0100

    'fff'

--- parkingBack/settings.py
+++ parkingBack/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'parkingBack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'parkingdb',
+        'USER': 'wordparkgames',
+        'PASSWORD': '4oeanULd6phigrb',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
